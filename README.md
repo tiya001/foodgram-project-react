284ab40164316c36425c99e31019eb9919a4e20d
Якупов Тимур <tiy001@yandex.ru>
2023-04-25 20:41:53 +0300

    deploy

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# praktikum_new_diplom
+![example workflow](https://github.com/tiya001/foodgram-project-react/actions/workflows/foodgram.yml/badge.svg)
+# foodgram-project-react
+foodgram
+130.193.37.18
+admin 
+login:1
+password:1
+email:1@1.aa
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,17 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('TOKEN', 'default-value')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ettctb)$%r8o#dmtgk9!cq7)509&ukb$zfq181!cca+uyhm8)+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
